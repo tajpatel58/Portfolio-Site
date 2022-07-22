d090ad01c05412e97579d665a4c438621108b9c9
tajpatel58 <tajpatel58@gmail.com>
2022-07-22 22:09:46 +0100

    Updating Main.JS

--- Website/js/main.js
+++ Website/js/main.js
@@ -129,7 +129,7 @@ class Chatbox{
         let msg1 = {name : "User", message : text1}
         this.messages.push(msg1);
         
-        fetch('http://localhost:8080' + '/predictions/chatbot', {
+        fetch('https://us-central1-aiplatform.googleapis.com/v1/projects/Website-Chatbot/locations/us-central1/endpoints/2781034342552436736:predict', {
             method: 'POST',
             body : JSON.stringify({message : text1}),
             mode : 'cors',
